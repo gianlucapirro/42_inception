c222ea6fac88d1c20fe812f7e5b5207dfef51e58
Your Name <you@example.com>
2023-03-29 22:20:27 +0200

    ready;

--- srcs/wordpress/setup.sh
+++ srcs/wordpress/setup.sh
@@ -1,7 +1,5 @@
+#!/bin/bash
 
-curl --insecure  -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-mv wp-cli.phar /usr/bin/wp-cli
-chmod +x /usr/bin/wp-cli
 wp-cli core install \
 	--allow-root \
 	--url=${WORDPRESS_URL} \
@@ -10,4 +8,9 @@ wp-cli core install \
 	--admin_password=${WORDPRESS_ADMIN_PASS} \
 	--admin_email=${WORDPRESS_ADMIN_EMAIL} \
 	--path=/var/www/html
+
+# /usr/sbin/php-fpm7.3 -F
+
+service php7.3-fpm start
+tail -f /dev/null
 
