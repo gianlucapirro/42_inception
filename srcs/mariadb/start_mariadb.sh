a344163457970977cf54227bba36a35f05363d0b
Gian Luca Pirro <87224325+gianlucapirro@users.noreply.github.com>
2023-03-29 17:38:31 +0200

    working wordpress with db

--- srcs/mariadb/start_mariadb.sh
+++ srcs/mariadb/start_mariadb.sh
@@ -4,9 +4,9 @@ set -e
 # Initialize MariaDB data directory if it's empty
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB data directory..."
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    mysql_install_db --user=mariadbuser --datadir=/var/lib/mysql
 fi
 
 # Start MariaDB server
 echo "Starting MariaDB server..."
-exec mysqld --user=mysql --console
+exec mysqld --user=mariadbuser --console
