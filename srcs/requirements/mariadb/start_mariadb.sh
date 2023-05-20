08d5a84a7ed455b6218856f518628da62ed23b13
Your Name <you@example.com>
2023-05-20 20:23:41 +0200

    change

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db --user=mariadbuser  --datadir=/var/lib/mysql
+    mysql_install_db --user=${MYSQL_USER}  --datadir=/var/lib/mysql
 fi
 
-exec mysqld --user=mariadbuser --console
+exec mysqld --user=${MYSQL_USER} --console
